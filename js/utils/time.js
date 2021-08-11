2d6bd6d404a9fe84d0e4465ea8ef9ce585eba826
Vicklleall <747856853@qq.com>
2021-08-11 20:12:49 +0800

    build.g5t0a

---
+++ js/utils/time.js
@@ -0,0 +1,1 @@
+const Time={pad:t=>(t<10?'0':'')+t,toString(t,i=!1){let r=this.pad(t/3600<<0)+this.pad(t%3600/60<<0)+this.pad(t%60<<0);return i&&(r+=(t%1).toFixed(2).slice(2)),r},formatMin(t){return`${this.pad(t/60<<0)}:${this.pad(t%60<<0)}:${this.pad(Math.round(t%1*100))}`},toNumber(t){let[i,r,e]=t.split(':');return 3600*i+60*r+Number(e)}};
